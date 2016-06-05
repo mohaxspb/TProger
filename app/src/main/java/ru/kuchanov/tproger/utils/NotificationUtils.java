b2acb3f069078fda30e0b0c1ddbc7a3caf274741
mohaxspb <mohax.spb@gmail.com>
2016-06-05 19:20:36 +0300

    -test commit

---
+++ app/src/main/java/ru/kuchanov/tproger/utils/NotificationUtils.java
@@ -0,0 +1,26 @@
+package ru.kuchanov.tproger.utils;
+
+import android.content.Context;
+
+/**
+ * Created by Юрий on 05.06.2016 19:14.
+ * For TProger.
+ */
+public class NotificationUtils
+{
+
+    public static void setAlarm(Context ctx)
+    {
+        //TODO
+    }
+
+    public static void cancelAlarm(Context ctx)
+    {
+        //TODO
+    }
+
+    public static void init(Context ctx)
+    {
+        //TODO
+    }
+}
